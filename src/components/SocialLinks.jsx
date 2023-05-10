a0f1d3d740ef4e8ef8563462a185ab2e2087f4e1
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-05-10 10:05:19 +0100

    CV updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/CV.pdf",
+      href: "/CV_GoncaloArsenio_23.pdf",
       style: "rounded-br-md",
       download: true,
     },
